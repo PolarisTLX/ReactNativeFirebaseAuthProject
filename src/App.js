fb1acc67f3ceb7fa57ec7006d5a2107c6e28f979
Paul Rail <paul.a.rail@gmail.com>
2019-07-03 00:13:37 -0400

    incorporate Firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
-  state = {  }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBwyOle0bzVoXS-MHZIYTsAhQCb01DadCk',
+      authDomain: 'reactnativefirebaseauthproject.firebaseapp.com',
+      databaseURL: 'https://reactnativefirebaseauthproject.firebaseio.com',
+      projectId: 'reactnativefirebaseauthproject',
+      storageBucket: '',
+      messagingSenderId: '699796752165',
+      appId: '1:699796752165:web:7310c5b2ce0422da'
+    });
+  }
+
   render() { 
     return ( 
       <View>
-        <Header headerText="Authentication" />
+        <Header headerText='Authentication' />
         <Text>Firebase Auth App</Text>
       </View>
      );
