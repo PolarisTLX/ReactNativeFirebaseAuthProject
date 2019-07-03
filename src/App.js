c1f296e0ac169063746b8dba73559eb8a4d41bc6
Paul Rail <paul.a.rail@gmail.com>
2019-07-03 12:33:27 -0400

    loggedIn state + switch case to show Login Form or Logout Button or Spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 import AsyncStorage from '@react-native-community/async-storage';
 
 class App extends Component {
+
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -17,13 +19,32 @@ class App extends Component {
       messagingSenderId: '699796752165',
       appId: '1:699796752165:web:7310c5b2ce0422da'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });        
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() { 
     return ( 
       <View>
         <Header headerText='Authentication' />
-        <LoginForm></LoginForm>
+        {this.renderContent()}
       </View>
      );
   }
