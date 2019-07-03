e67ea9f1f0316a97f408f38039a5155ec0e80034
Paul Rail <paul.a.rail@gmail.com>
2019-07-02 23:53:14 -0400

    bring over common components from previous project and connect them all with special index.js file + get a use case working with Header component

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+// this file is used as a trick to get access to several common components
+// (common accross several apps)
+// it must be named index.js to work
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+
+// the export statement from those files must also be changed, see those files
